a737f683eeaa471082dddf84557cd68a196023fe
Christian Kokott <christian.kokott@googlemail.com>
2016-11-11 23:48:08 +0100

    Snake of love example

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ function init() {
 
 }
 
-var bgColor = 'rgba(0, 0, 0, 0.05)';
-var x = 20;
+var bgColor = 'rgba(0, 0, 0, 0.9)';
 function draw(dt) {
   ctx.fillStyle = bgColor;
   ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-  x += dt * 20;
-  fillCircle(x, 20, 10, 'white');
+  ctx.save();
+
+  ctx.translate((WIDTH / 2) - (75), (HEIGHT / 2));
+  //fillHeart();
+
+  const heartCount = 20;
+  for(let i = 0; i < heartCount; i++) {
+    //ctx.scale(0.95 + Math.cos(totalSeconds) * 0.01, 0.95  + Math.cos(totalSeconds) * 0.05);
+    ctx.scale(0.95, 0.95);
+    let offset = (360 / heartCount) * i;
+
+    ctx.translate(Math.cos(totalSeconds * 1 * i) * 10, 5 + Math.sin(10 + totalSeconds * 2) * 10);
+    fillHeart(HSLA(0, offset * Math.cos(totalSeconds * 0.4) * 5));
+  }
+
+
+  ctx.restore();
 }
